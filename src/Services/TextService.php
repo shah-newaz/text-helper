d7019c90b50639a1435662eba76560dfa6c14881
Shah <rifat_rifats@live.com>
2017-10-21 22:01:19 +0600

    word count
    - word count

--- src/Services/TextService.php
+++ src/Services/TextService.php
@@ -6,4 +6,9 @@ class TextService {
 	public function makeTitle ($text) {
 		return title_case($text);
 	}
+
+	public function wordCount ($text) {
+		$array = explode(' ', $text);
+		return 'The sentence has '. count($array).' words.';
+	}
 }
